7d1de47c53f57a5343dce131d62bb22be6b7b843
Harrison Glover <harrison_glover@yahoo.com>
2020-10-25 22:40:37 -0500

    added event for saving schedule content to local storage and logic to retrieve schedule from local stroage. Added logic to compare current time to schedule time. Added logic to display current date.

---
+++ script.js
@@ -0,0 +1,62 @@
+var currentDay = $("#currentDay");
+var containerEl = $(".container");
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $(".saveBtn").on("click",function(){
+       var value = $(this).siblings(".description").val();
+       var time = $(this).parent().attr("id");
+       localStorage.setItem(time,value);
+    });
+
+    function compareHour(){
+        var currentHour = moment().hours();
+        
+        $(".time-block").each(function(){
+            var schedTime = parseInt($(this).attr("id").split("-")[1]);
+            console.log(schedTime);
+        
+
+        if (schedTime < currentHour){
+            $(this).addClass("past");
+        }   
+        else if (schedTime === currentHour){
+          $(this).removeClass("past");
+          $(this).addClass(".present");  
+        }   
+        else {
+            $(this).removeClass("past");
+            removeClass("present");
+            addClass("future");
+        }
+
+        
+        
+        });
+    }
+        compareHour();
+
+        var time = setInterval(compareHour, 15000);
+
+    
+
+
+currentDay.html(moment().format('LL'));
+
+$("#hr-9 .description").val(localStorage.getItem("hr-9"));
+$("#hr-10 .description").val(localStorage.getItem("hr-10"));
+$("#hr-11 .description").val(localStorage.getItem("hr-11"));
+$("#hr-12 .description").val(localStorage.getItem("hr-12"));
+$("#hr-13 .description").val(localStorage.getItem("hr-13"));
+$("#hr-14 .description").val(localStorage.getItem("hr-14"));
+$("#hr-15 .description").val(localStorage.getItem("hr-15"));
+$("#hr-16 .description").val(localStorage.getItem("hr-16"));
+$("#hr-17 .description").val(localStorage.getItem("hr-17"));
+
+});
